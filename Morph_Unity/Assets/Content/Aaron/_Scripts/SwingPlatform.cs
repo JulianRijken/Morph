58631a57ecb4a146f01c7956f2181928f2f642ae
Aaron Knoop <aaron.knoop@live.nl>
2020-03-10 10:25:15 +0100

    Added code based animation for the platform
    TODO Add damping

--- Morph_Unity/Assets/Content/Aaron/_Scripts/SwingPlatform.cs
+++ Morph_Unity/Assets/Content/Aaron/_Scripts/SwingPlatform.cs
@@ -1,30 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SwingPlatform : MonoBehaviour
 {
 	[Header("Platform settings")]
+	[SerializeField] Transform _AnchorPoint;
+	[SerializeField] Transform _Platform;
+	[SerializeField] bool _Animated;
+	[SerializeField] float _SwingSpeed;
+	[SerializeField] Vector3 _SwingAngle;
+
+	[Header("Line settings")]
 	[SerializeField] Transform _CenterPoint;
 	[SerializeField] Transform[] _LinePoints;
 	[SerializeField] LineRenderer _LineRenderer;
-	List<Vector3> _Points = new List<Vector3>();
 
+	readonly List<Vector3> _Points = new List<Vector3>();
+	bool _SwingForward = true;
+	float _T;
+	
 	void Start()
 	{
 		_LineRenderer.positionCount = (_LinePoints.Length * 2);
+		if (!_Animated && TryGetComponent(out Animator anim))
+		{
+			anim.enabled = false;
+		}
 	}
 
 	void Update()
 	{
-		SwingHandle();
+		if(!_Animated) SwingHandle();
+		LineHandle();
 	}
 
-	void SwingHandle()
+	void LineHandle()
 	{
 		if (_LineRenderer == null) return;
-		
+
 		_Points.Clear();
 
 		for (int i = 0; i < _LinePoints.Length; i++)
@@ -34,7 +47,21 @@ public class SwingPlatform : MonoBehaviour
 		}
 
 		_LineRenderer.SetPositions(_Points.ToArray());
-		// _LineRenderer.positionCount = _Points.Count;
-		// _LineRenderer.SetPositions(_Points.ToArray());
+	}
+
+	//TODO Add damping
+	void SwingHandle()
+	{
+		_AnchorPoint.eulerAngles = Vector3.Lerp(-_SwingAngle, _SwingAngle, _T);
+		_Platform.localEulerAngles = _AnchorPoint.localEulerAngles * -1;
+		if (_T >= 1)
+		{
+			_SwingForward = false;
+		} else if (_T <= 0)
+		{
+			_SwingForward = true;
+		}
+		
+		_T = _SwingForward ? _T + Time.deltaTime * _SwingSpeed :_T - Time.deltaTime * _SwingSpeed;
 	}
 }
