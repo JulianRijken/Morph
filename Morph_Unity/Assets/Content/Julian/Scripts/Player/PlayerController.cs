fb8104ef9e240211ab84f917c40322ceed5fb5fa
Julian Rijken <Rijkenjulian@gmail.com>
2020-03-24 17:06:29 +0100

    Fuck i ben de helen dag bezig geweest maar kom er niet uit kut player

--- Morph_Unity/Assets/Content/Julian/Scripts/Player/PlayerController.cs
+++ Morph_Unity/Assets/Content/Julian/Scripts/Player/PlayerController.cs
@@ -6,223 +6,106 @@ using UnityEngine.InputSystem;
 namespace Morph.Julian
 {
 
-    //===== PlayerController made by Julian Rijken ====\\
-    // Todo fix: 
-    // Head Collision for Jumping
-    // Wierd bounce when jumping up against steep slopes
-    // Input stopping when up against slopes without bouncing when running against slopes
-
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D), typeof(Animator))]
     public class PlayerController : MonoBehaviour
     {
-
-        [Header("Collision")]
-        [SerializeField] private float m_groundCheckDistance;
-        [SerializeField] private LayerMask m_groundLayer;
-
-        [Header("Movement")]
-        [SerializeField] private PlayerSettings m_blobMovementSettings;
-        [SerializeField] private PlayerSettings m_birdMovementSettings;
-
+        [Header("Componenets")]
+        [SerializeField] private Transform m_spritesParent;
         private Rigidbody2D m_rigidbody2D;
         private BoxCollider2D m_boxCollider2D;
         private Animator m_animatior;
 
-        private float m_strafeInput;
-        private float m_smoothStrave;
+        [Header("Movement")]
+        [SerializeField] private float m_strafeSpeed;
+        private Vector2 m_velocity;
         private float m_gravity;
-        private float m_jumpForce;
-        private bool m_inJump;
-        private bool m_jumpAllowed;
-        private float m_jumpTime = 0f;
-        private Vector2 m_slideVelocity;
-        private Vector2 m_inputVelocity;
-        private Dictionary<PlayerType,PlayerSettings> m_playerSettings = new Dictionary<PlayerType, PlayerSettings>();
-        private PlayerType m_activePlayerType;
-        private enum PlayerType{ blob = 0, bird = 1 }
-        private PlayerSettings Settings { get => m_playerSettings[m_activePlayerType]; }
 
-        private void Start()
+        [Header("Input")]
+        private float m_strafeInput;
+
+        [Header("Collision")]
+        [SerializeField] private float m_groundCheckDistance;
+        [SerializeField] private LayerMask m_groundLayer;
+        private float m_groundHitDistance;
+        private Vector2 m_groundOrgin { get => (Vector2)transform.position + m_boxCollider2D.offset; }
+
+        private void Awake()
         {
             m_rigidbody2D = GetComponent<Rigidbody2D>();
             m_boxCollider2D = GetComponent<BoxCollider2D>();
             m_animatior = GetComponent<Animator>();
-
-            m_playerSettings.Add(PlayerType.blob, m_blobMovementSettings);
-            m_playerSettings.Add(PlayerType.bird, m_birdMovementSettings);
-
-            SetPlayeType(PlayerType.bird);
-
-            if (Settings.m_jumplength <= 0f)
-                Settings.m_jumplength = 1f;
         }
-        private void Update()
-        {
-            #if UNITY_EDITOR
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-                SetPlayeType(PlayerType.blob);
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-                SetPlayeType(PlayerType.bird);
-            #endif
-        }
+
         private void FixedUpdate()
         {
-            HandlePlayerMovement();
-        }
-        private void OnCollisionExit2D(Collision2D collision)
-        {
-            SlowDownInput(collision);
-        }
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            SlowDownInput(collision);
-
-            if (collision.contacts[0].normal.y < 0f)
-                CancelJump(false);
+            MovementUpdate();
         }
 
 
+        #region Input
         public void OnStrafe(InputAction.CallbackContext context)
         {
             m_strafeInput = context.ReadValue<float>();
+
+            Vector3 flipPos = m_spritesParent.localScale;
+            if (m_strafeInput > 0)
+                flipPos.x = 1;
+            else if (m_strafeInput < 0)
+                flipPos.x = -1;
+            m_spritesParent.localScale = flipPos;
         }
         public void OnJump(InputAction.CallbackContext context)
         {
-            if (context.performed && m_jumpAllowed && !m_inJump && Settings.m_canJump)
-                StartCoroutine(JumpCourutine());
+            //if (context.performed && m_jumpAllowed && !m_inJump && Settings.m_canJump)
+            //    StartCoroutine(JumpCourutine());
         }
+        #endregion
 
 
-        private void HandlePlayerMovement()
+        private void MovementUpdate()
         {
+            // Get the ground hit
             RaycastHit2D hit = GroundCheck();
             float angle = Vector2.Angle(hit.normal, Vector2.up);
 
-            bool isAccelerating = (m_smoothStrave > 0f && m_strafeInput > 0f) || (m_smoothStrave < 0f && m_strafeInput < 0f) ? true : false;
+            // Get distance
+            m_groundHitDistance = m_groundOrgin.y - hit.point.y - (m_boxCollider2D.size.y / 2f);
 
-            m_gravity += Settings.m_gravityForce * Time.fixedDeltaTime;
+            // Get strave input
+            float strave = m_strafeInput;
+            strave *= m_strafeSpeed;
 
-            // if on ground
-            if (hit.collider != null && hit.distance < 0.5f)
-            {
+            // Apply gravity
+            m_gravity -= 9f * Time.fixedDeltaTime;
 
-                // if on slope
-                if (angle > Settings.m_slopeLimit)
-                {
-                    m_jumpAllowed = false;
+            Vector2 dirVelocity = (strave * (Quaternion.Euler(0, 0, -90f) * hit.normal));
 
-                    if (hit.normal.x > 0)
-                    {
-                        m_strafeInput = Mathf.Clamp(m_strafeInput, 0, 1);
-
-                        if (m_strafeInput == 0)
-                            m_slideVelocity -= (Vector2)(Quaternion.Euler(0, 0, 90f) * hit.normal) * Settings.m_slideSpeed * Time.fixedDeltaTime;
-
-                    }
-                    else
-                    {
-                        m_strafeInput = Mathf.Clamp(m_strafeInput, -1, 0);
-
-                        if (m_strafeInput == 0)
-                            m_slideVelocity += (Vector2)(Quaternion.Euler(0, 0, 90f) * hit.normal) * Settings.m_slideSpeed * Time.fixedDeltaTime;
-                    }
-
-                }
-                else
-                {
-                    m_jumpAllowed = true;
-                }
-
-                // Set the player to the ground
-                if (hit.distance <= 0.05f)
-                {
-                    m_gravity = 0;
-                }
+            Vector2 move = m_velocity + dirVelocity + (Vector2.up * m_gravity);
+            //m_rigidbody2D.velocity = move;
+            m_rigidbody2D.MovePosition(m_rigidbody2D.position + move * Time.fixedDeltaTime);
+            // ZORG DAT DE GRAVITY STOP ALS JE OP DE GROND STAAT 
 
 
-                // if on ground and falling
-                if (m_jumpTime > Settings.m_jumpMidTime)
-                    CancelJump(false);
 
-                if (angle > 0 && m_jumpTime > 0.1f)
-                    CancelJump(false, Settings.m_slopeJumpCancelForce);
-
-                m_smoothStrave = Mathf.MoveTowards(m_smoothStrave, m_strafeInput * Settings.m_maxStrafeSpeed, Time.fixedDeltaTime * (isAccelerating ? Settings.m_accelerateSpeed : Settings.m_deccelerateSpeed));
-                m_inputVelocity = (-m_smoothStrave * (Quaternion.Euler(0, 0, 90f) * hit.normal));
-
-            }
-            else // if in air
-            {
-                m_jumpAllowed = false;
-
-                m_smoothStrave = Mathf.MoveTowards(m_smoothStrave, m_strafeInput * Settings.m_maxStrafeSpeed, Time.fixedDeltaTime * (isAccelerating ? Settings.m_accelerateSpeed : Settings.m_deccelerateSpeed));
-                m_inputVelocity.x = m_smoothStrave;
-
-                m_inputVelocity.y = 0;
-            }
-
-
-            m_slideVelocity = Vector2.MoveTowards(m_slideVelocity, Vector2.zero, Time.fixedDeltaTime * Settings.m_slopeSlowDownSpeed);
-
-            m_rigidbody2D.velocity = m_inputVelocity + m_slideVelocity + (Vector2.down * (m_inJump ? m_jumpForce : m_gravity));
-
-
-            Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * m_smoothStrave, isAccelerating ? Color.red : Color.green);
+#if UNITY_EDITOR
+            // Debug
+            Debug.Log(m_rigidbody2D.velocity.y);
+            Debug.DrawLine(m_groundOrgin, hit.point,Color.red);
+            Debug.DrawRay(transform.position,Vector2.right * strave);
+            #endif
         }
-        private void SlowDownInput(Collision2D collision)
+
+
+        private void OnCollisionStay2D(Collision2D collision)
         {
-            if (collision.contactCount > 0)
-            {
-                //Debug.Log(Mathf.Abs(collision.contacts[0].normal.x));
-
-                if (Mathf.Abs(collision.contacts[0].normal.x) >= 0.7f)
-                    m_smoothStrave = 0f;
-            }
-        }
-        private void CancelJump(bool keepForce = true, float extraForce = 0)
-        {
-            if (m_inJump == true)
-            {
-                if (keepForce)
-                    m_gravity = m_jumpForce;
-                else
-                    m_gravity = 0;
-
-                m_gravity += extraForce;
-
-                m_inJump = false;
-            }
+            if (m_groundHitDistance < 0.1f)
+                m_gravity = 0;
         }
 
 
-        private void SetPlayeType(PlayerType type) 
-        {
-            m_activePlayerType = type;
-            m_animatior.SetInteger("playerType", (int)type);
-            CancelJump(true);
-        }
-
-        private IEnumerator JumpCourutine()
-        {
-            m_inJump = true;
-            m_jumpTime = 0f;
-
-            while (m_jumpTime <= Settings.m_jumplength && m_inJump == true)
-            {
-                float time = m_jumpTime / Settings.m_jumplength;
-
-                m_jumpForce = Mathf.Lerp(1, 0, Settings.m_jumpCurve.Evaluate(time)) * Settings.m_jumpHeight * (time <= Settings.m_jumpMidTime ? -1 : 1);
-                m_jumpTime += Time.deltaTime;
-                yield return new WaitForEndOfFrame();
-            }
-
-            CancelJump();
-        }
         private RaycastHit2D GroundCheck()
         {
-            return Physics2D.BoxCast((Vector2)transform.position + m_boxCollider2D.offset, m_boxCollider2D.size, 0, Vector2.down, m_groundCheckDistance, m_groundLayer);
+            return Physics2D.BoxCast(m_groundOrgin, m_boxCollider2D.size, 0, Vector2.down, m_groundCheckDistance, m_groundLayer);
         }
-
     }
-
 }
