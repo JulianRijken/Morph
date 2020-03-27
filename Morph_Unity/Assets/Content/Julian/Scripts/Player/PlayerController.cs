36e0ef023618e8db45c4eb149076675e9edf8056
Julian Rijken <Rijkenjulian@gmail.com>
2020-03-27 15:29:40 +0100

    Player Version Two 4.30 uur werk

--- Morph_Unity/Assets/Content/Julian/Scripts/Player/PlayerController.cs
+++ Morph_Unity/Assets/Content/Julian/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,14 +12,22 @@ namespace Morph.Julian
         [SerializeField] private Transform m_spritesParent;
         private Rigidbody2D m_rigidbody2D;
         private BoxCollider2D m_boxCollider2D;
+        private CircleCollider2D m_circleCollider2D;
         private Animator m_animatior;
 
         [Header("Movement")]
         [SerializeField] private float m_strafeSpeed;
         [SerializeField] private float m_gravityForce;
+        [SerializeField] private PlayerState m_playerState;
+        [SerializeField] private float m_drag;
         private Vector2 m_velocity;
         private float m_gravity;
-        private bool m_inAir;
+        private bool m_holdingAbility;
+
+        private enum PlayerState {BlobGround, BlobAir, BlobBall, BirdAir }
+
+        [Header("BallMode")]
+        [SerializeField] private float m_ballGravity;
 
         [Header("Input")]
         private float m_strafeInput;
@@ -34,12 +41,14 @@ namespace Morph.Julian
         private float m_groundHitAngle;
         private Vector2 m_groundOrgin { get => (Vector2)transform.position + m_boxCollider2D.offset; }
 
+
         #region UnityFunctions
 
         private void Awake()
         {
             m_rigidbody2D = GetComponent<Rigidbody2D>();
             m_boxCollider2D = GetComponent<BoxCollider2D>();
+            m_circleCollider2D = GetComponent<CircleCollider2D>();
             m_animatior = GetComponent<Animator>();
         }
 
@@ -61,7 +70,11 @@ namespace Morph.Julian
             }
             else if (collision.contacts[0].normal.y > 0f)
             {
-                m_inAir = false;
+                if (m_playerState == PlayerState.BlobBall && m_holdingAbility)
+                    return;
+
+                SwitchMode(PlayerState.BlobGround);
+                     
             }
         }
 
@@ -92,14 +105,38 @@ namespace Morph.Julian
         }
         public void OnAbility(InputAction.CallbackContext context)
         {
-            if (context.performed && !OnGround() && !OnMaxSlope())
-                DoSmash();
+            if (context.performed)
+            {
+                m_holdingAbility = true;
+
+                if (m_playerState != PlayerState.BlobBall)
+                {
+                    if (m_playerState == PlayerState.BlobGround)
+                    {
+                        SwitchMode(PlayerState.BlobBall);
+                    }
+                    else
+                    {
+                        SwitchMode(PlayerState.BlobBall);
+                        DoSmash();
+                    }
+                }
+            }
+            else if (context.canceled)
+            {
+                m_holdingAbility = false;
+
+                if (m_playerState == PlayerState.BlobBall)
+                {
+                    SwitchMode(PlayerState.BlobGround);
+                }
+            }
 
         }
 
         #endregion
 
-        #region Functions
+
 
         private void HandleSpriteRotation()
         {
@@ -108,73 +145,118 @@ namespace Morph.Julian
         private void DoJump()
         {
             m_gravity = 10;
-            m_inAir = true;
+            SwitchMode(PlayerState.BlobAir);
         }
 
         private void DoSmash()
         {
-            Debug.Log("Do Smash");
-            m_gravity -= 20;
-            
+            m_rigidbody2D.AddForce(Vector2.down * 100, ForceMode2D.Impulse);     
+        }
+
+        private void BecomeBall()
+        {
+            Debug.Log("Become Ball");
+            m_boxCollider2D.enabled = false;
+            m_circleCollider2D.enabled = true;
+            m_rigidbody2D.constraints = RigidbodyConstraints2D.None;
+            m_rigidbody2D.gravityScale = m_ballGravity;
+        }
+
+        private void BecomeNormal()
+        {
+            Debug.Log("Become Normal");
+
+            m_boxCollider2D.enabled = true;
+            m_circleCollider2D.enabled = false;
+            m_rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+            m_rigidbody2D.gravityScale = 0;
+            StartCoroutine(RotateBack());
+            m_velocity = m_rigidbody2D.velocity;
+            m_gravity = 0f;
+        }
+
+        private IEnumerator RotateBack()
+        {
+            float time = 0;
+            Quaternion oldRot = transform.rotation;
+            while (time < 1f)
+            {
+                time += Time.deltaTime * 4f;
+                transform.rotation = Quaternion.Slerp(oldRot, Quaternion.identity, time);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         private void MovementUpdate()
         {
-            // Get the ground hit
-            RaycastHit2D hit = GroundCheck();
-            m_groundHitAngle = Vector2.Angle(hit.normal, Vector2.up);
 
-            // Get distance
-            m_groundHitDistance = m_groundOrgin.y - hit.point.y - (m_boxCollider2D.size.y / 2f);
-
-            // Get strave input
-            float strave = m_strafeInput;
-            strave *= m_strafeSpeed;
-
-            // Get Move Velocity
-            Vector2 straveVeclocity = Vector2.zero;
-
-            if (OnGround() && !OnMaxSlope())
+            if (m_playerState == PlayerState.BlobGround || m_playerState == PlayerState.BlobAir)
             {
-                // IF on ground
-                m_gravity = 0f;
-                straveVeclocity = (strave * (Quaternion.Euler(0, 0, -90f) * hit.normal));
+
+                // Get the ground hit
+                RaycastHit2D hit = GroundCheck();
+                m_groundHitAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+                // Get distance
+                m_groundHitDistance = m_groundOrgin.y - hit.point.y - (m_boxCollider2D.size.y / 2f);
+
+                // Get strave input
+                float strave = m_strafeInput;
+                strave *= m_strafeSpeed;
+
+                // Get Move Velocity
+                Vector2 straveVeclocity = Vector2.zero;
+
+                if (OnGround() && !OnMaxSlope())
+                {
+                    // IF on ground
+                    m_gravity = 0f;
+                    straveVeclocity = (strave * (Quaternion.Euler(0, 0, -90f) * hit.normal));
+                }
+                else
+                {
+                    // IF in air
+
+                    // Apply gravity
+                    m_gravity -= m_gravityForce * Time.fixedDeltaTime;
+                    straveVeclocity = Vector2.right * strave;
+                }
+
+
+
+                m_velocity = Vector2.MoveTowards(m_velocity, Vector2.zero, Time.fixedDeltaTime * m_drag);
+                Vector2 move = m_velocity + straveVeclocity + (Vector2.up * m_gravity);
+                m_rigidbody2D.velocity = move;
+                //m_rigidbody2D.MovePosition(m_rigidbody2D.position + move * Time.fixedDeltaTime);
+                // ZORG DAT DE GRAVITY STOP ALS JE OP DE GROND STAAT 
+
             }
-            else
-            {
-                // IF in air
 
-                // Apply gravity
-                m_gravity -= m_gravityForce * Time.fixedDeltaTime;
-                straveVeclocity = Vector2.right * strave;
-            }
-
-
-    
-
-            Vector2 move = m_velocity + straveVeclocity + (Vector2.up * m_gravity);
-            m_rigidbody2D.velocity = move;
-            //m_rigidbody2D.MovePosition(m_rigidbody2D.position + move * Time.fixedDeltaTime);
-            // ZORG DAT DE GRAVITY STOP ALS JE OP DE GROND STAAT 
-
-
-
-            #if UNITY_EDITOR
-            // Debug
-            // Debug.Log(m_rigidbody2D.velocity.y);
-            // Debug.Log(OnMaxSlope());
-            Debug.DrawLine(m_groundOrgin, hit.point,Color.red);
-            Debug.DrawRay(transform.position,Vector2.right * strave);
-            #endif
         }
 
-        #endregion
+        private void SwitchMode(PlayerState state)
+        {
+            if (state == PlayerState.BlobGround || state == PlayerState.BlobAir)
+            {
+                if (m_playerState == PlayerState.BlobBall)
+                {
+                    BecomeNormal();
+    
+                }
+            }
+            else if(state == PlayerState.BlobBall)
+            {
+                BecomeBall();
+            }
 
-        #region ReturnFuctions
+
+            m_animatior.SetInteger("PlayerState", (int)state);
+            m_playerState = state;
+        }
 
         private bool OnGround()
         {
-            return (m_groundHitDistance < m_groundFether && m_inAir == false) ? true : false;
+            return (m_groundHitDistance < m_groundFether && m_playerState == PlayerState.BlobGround) ? true : false;
         }
 
         private bool OnMaxSlope()
@@ -187,7 +269,11 @@ namespace Morph.Julian
             return Physics2D.BoxCast(m_groundOrgin, m_boxCollider2D.size, 0, Vector2.down, m_groundCheckDistance, m_groundLayer);
         }
 
-        #endregion
+
+
+
+
+
 
     }
 }
