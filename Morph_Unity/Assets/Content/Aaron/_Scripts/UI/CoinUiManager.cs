71279a768142dc402ee65e4b1c7a47721036d8ac
Aaron Knoop <aaron.knoop@live.nl>
2020-04-03 00:41:09 +0200

    Updated CoinUIManager

--- Morph_Unity/Assets/Content/Aaron/_Scripts/UI/CoinUiManager.cs
+++ Morph_Unity/Assets/Content/Aaron/_Scripts/UI/CoinUiManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using com.Morph.Game;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +10,8 @@ public class CoinUiManager : MonoBehaviour
 	[SerializeField] Vector2 _Offset;
 	[SerializeField] Transform _Spawn;
 	[SerializeField]Vector3[] _Corners = new Vector3[4];
-	List<CoinUi> _Coins = new List<CoinUi>();
+	Stack<CoinUi> _Coins = new Stack<CoinUi>();
+	Stack<CoinUi> _CoinsUse = new Stack<CoinUi>();
 	void Start()
 	{
 		GameManager.GetInstance()._OnCoinAdded += OnCoinAdded;
@@ -23,12 +25,38 @@ public class CoinUiManager : MonoBehaviour
 			_Corners[i].y = 0;
 		}
 		
-		_Coins.Add(Instantiate(_CoinUiPrefab, _Spawn.position, Quaternion.identity, transform).GetComponent<CoinUi>());
+		_Coins.Push(Instantiate(_CoinUiPrefab, _Spawn.position, Quaternion.identity, transform).GetComponent<CoinUi>());
 		for (int i = 0; i < newAmmount; i++)
 		{
 			Vector3 targetPos = Vector3.Lerp(Vector3.zero, _Corners[2], (1f / (newAmmount+1)) * (i+1));
 			targetPos.y = _Offset.y;
-			_Coins[i].SetNewPosition(targetPos);
+			_Coins.ToList()[i].SetNewPosition(targetPos);
+		}
+	}
+
+	public bool UseCoin(Transform targetTransform)
+	{
+		if (_Coins.Peek() == null) return false;
+		
+		_CoinsUse.Push(_Coins.Pop());
+		CoinUi ui = _CoinsUse.Peek();
+		ui.SetFollowTransform(targetTransform);
+
+		return true;
+	}
+	
+	public void ResetCoins()
+	{
+		for (int i = 0; i < _CoinsUse.Count; i++)
+		{
+			_Coins.Push(_CoinsUse.Pop());
+		}
+		
+		for (int i = 0; i < _Coins.Count; i++)
+		{
+			Vector3 targetPos = Vector3.Lerp(Vector3.zero, _Corners[2], (1f / (_Coins.Count)) * (i+1));
+			targetPos.y = _Offset.y;
+			_Coins.ToList()[i].SetNewPosition(targetPos);
 		}
 	}
 }
