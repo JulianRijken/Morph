757163ce836c87f798f1fc1ab6d45c6bd99e692f
Julian Rijken <Rijkenjulian@gmail.com>
2020-04-07 11:42:40 +0200

    Player Old Removed + BouncePad

---
+++ Morph_Unity/Assets/Bouncepad.cs
@@ -0,0 +1,20 @@
+﻿using Morph.Julian;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncepad : MonoBehaviour
+{
+
+    [SerializeField] private float m_bounceForce = 40f;
+    [SerializeField] private bool m_bounceInPadDirection = true;
+    [SerializeField] private Vector2 m_bouceDirection = Vector2.up;
+    [SerializeField] private ForceMode2D m_forceMode = ForceMode2D.Impulse;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D playerRigidbody = collision.collider.GetComponent<Rigidbody2D>();
+        if(playerRigidbody != null)     
+            playerRigidbody.AddForce((m_bounceInPadDirection ? (Vector2)transform.up : m_bouceDirection).normalized * m_bounceForce, m_forceMode);     
+    }
+}
