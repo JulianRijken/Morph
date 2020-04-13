14ef0dcc6efd9f987450199fd7e7a3d407703f6d
Aaron Knoop <aaron.knoop@live.nl>
2020-04-13 14:25:47 +0200

    Cleared windbox

--- Morph_Unity/Assets/Content/Aaron/_Scripts/Mechanics/WindBox.cs
+++ Morph_Unity/Assets/Content/Aaron/_Scripts/Mechanics/WindBox.cs
@@ -24,45 +24,7 @@ namespace com.Morph.Mechanics
 			{
 				if (col.TryGetComponent(out Rigidbody2D rig))
 					rig.AddForce(transform.right * (_WindStrength * 10), ForceMode2D.Force);
-				
-				print("A");
 			});
 		}
-
-#if UNITY_EDITOR
-
-		void OnDrawGizmos()
-		{
-			#region Box
-		
-			Gizmos.DrawWireCube(transform.position, transform.localScale);
-		
-			#endregion
-		
-			// #region Arrow
-			//
-			// Vector2 start = transform.position;
-			// start.x -= 0.5f;
-			// Vector2 end = transform.position;
-			// end.x += 0.25f;
-			//
-			// Vector2 startA = transform.position;
-			// startA.x -= .25f;
-			// startA.y -= .5f;
-			// Vector2 startB = transform.position;
-			// ;
-			// startB.x -= .25f;
-			// startB.y += .5f;
-			//
-			// Gizmos.color = Color.magenta;
-			// Gizmos.DrawLine(start, end);
-			// Gizmos.DrawLine(startA, end);
-			// Gizmos.DrawLine(startB, end);
-			//
-			// #endregion
-			
-		}
-
-#endif
 	}
 }
